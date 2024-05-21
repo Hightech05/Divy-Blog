71a55553937b82aa4f3d903b3a51a38bbfdaee72
unknown <hightechgaming25@gmail.com>
2024-05-21 21:59:47 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+/* If any body is building this project now then replace 
+the 'createEmailSession' method by
+ 'createEmailPasswordSession'. Because it will show an error 
+that 'createEmailSession' is not a function. Actually appwrite 
+updated the function but forgot to update the documentation.*/
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
